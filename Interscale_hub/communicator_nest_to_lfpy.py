7a7c3c9c076e70a2ece4fc7763e8b9638ac33578
mfahdaz <fahadazeemi@gmail.com>
2023-04-05 16:11:00 +0000

    added manager and communicator for nest to lfpy

---
+++ Interscale_hub/communicator_nest_to_lfpy.py
@@ -0,0 +1,220 @@
+# ------------------------------------------------------------------------------
+#  Copyright 2020 Forschungszentrum Jülich GmbH
+# "Licensed to the Apache Software Foundation (ASF) under one or more contributor
+#  license agreements; and to You under the Apache License, Version 2.0. "
+#
+# Forschungszentrum Jülich
+#  Institute: Institute for Advanced Simulation (IAS)
+#    Section: Jülich Supercomputing Centre (JSC)
+#   Division: High Performance Computing in Neuroscience
+# Laboratory: Simulation Laboratory Neuroscience
+#       Team: Multi-scale Simulation and Design
+#
+# ------------------------------------------------------------------------------ 
+
+from mpi4py import MPI
+import time
+import numpy as np
+
+from EBRAINS_InterscaleHUB.Interscale_hub.communicator_base import BaseCommunicator
+from EBRAINS_InterscaleHUB.Interscale_hub import interscalehub_utils
+from EBRAINS_InterscaleHUB.Interscale_hub.interscalehub_enums import DATA_BUFFER_STATES
+
+from EBRAINS_RichEndpoint.application_companion.common_enums import Response
+
+
+class CommunicatorNestLFPY(BaseCommunicator):
+    '''
+    Implements the BaseCommunicator. It 
+    1) Receives the data from NEST
+    2) Transform to LFPy signals
+    3) Save to a file
+    '''
+    def __init__(self, configurations_manager, log_settings,
+                 data_buffer_manager, mediator):
+        '''
+        '''
+        # initialize the common settings such as logger, data buffer, etc.
+        super().__init__(configurations_manager,
+                         log_settings,
+                         __name__,
+                         data_buffer_manager,
+                         mediator)
+        
+        self._logger.info("Initialized")
+      
+    def start(self, intra_communicator, inter_comm_receiver, inter_comm_sender):
+        '''
+        implements the abstract method to start
+        1) receiving the data
+        2) transforming the data to required scale
+        3) sending the transformed data
+        
+        M:N mapping of MPI ranks, receive data, further process data.
+        Receive on rank 0, do the rest on rest of the ranks.
+        '''
+        # Rank-0 will receive the data
+        if intra_communicator.Get_rank() == 0:
+            # set inter_communicator for receiving the data
+            self._comm_receiver = inter_comm_receiver
+            self._num_sending = self._comm_receiver.Get_remote_size()
+            return self._receive()
+
+        # NOTE its, one-way communication at the moment
+        # Rank-1 will transform and send the data
+        elif intra_communicator.Get_rank() == 1:
+            # set inter_communicator for sending the data
+            self._comm_sender = inter_comm_sender
+            return self._transform()
+
+    def stop(self):
+        '''
+        TODO: proper execution of stop command
+        '''
+        # self.__stop = True
+        try:
+            raise NotImplementedError
+        except NotImplementedError:
+            self._logger.exception("stop() is not implemented yet")
+            return Response.OK
+
+    def _receive(self):
+        '''
+        Receives data on rank 0. Put it into the shared mem buffer.
+        Replaces the former 'receive' function.
+        NOTE: First refactored version -> not pretty, not final. 
+        '''
+        # The last two buffer entries are used for shared information
+        # --> they replace the status_data variable from previous version
+        # --> find more elegant solution?
+        self._logger.info("setting up buffers")
+
+        # set buffer to 'ready to receive from nest'
+        # self.__databuffer[-1] = 1
+        self._data_buffer_manager.set_ready_at(index=-1)
+
+        # marks the 'head' of the buffer
+        # self.__databuffer[-2] = 0
+        self._data_buffer_manager.set_header_at(index=-2)
+
+        # It seems the 'check' variable is used to receive tags from NEST,
+        # i.e. ready for send...
+        # change this in the future, also mentioned in the FatEndPoint solution
+        # from Wouter.
+        check = np.empty(1,dtype='b')
+        shape = np.empty(1, dtype='i')    
+        count = 0
+        status_ = MPI.Status()
+        self._logger.info("reading from buffer")
+        while True:
+            running_head = 0  # head of the buffer, reset after each iteration
+            # TODO: This is still not correct. We only check for the Tag of the last rank.
+            # IF all ranks send always the same tag in one iteration (simulation step)
+            # then this works. But it should be handled differently!!!!
+            self._comm_receiver.Recv([check, 1, MPI.CXX_BOOL], source=0, tag=MPI.ANY_TAG, status=status_)
+
+            status_rank_0 = status_.Get_tag()
+            for i in range(1, self._num_sending):
+                # new: We do not care which source sends first, give MPI the freedom to send in whichever order.
+                # self._comm_receiver.Recv([check, 1, MPI.CXX_BOOL], source=MPI.ANY_SOURCE, tag=MPI.ANY_TAG, status=status_)
+                self._comm_receiver.Recv([check, 1, MPI.CXX_BOOL], source=i, tag=MPI.ANY_TAG, status=status_)
+                # Check if the state of the NEST is different between the ranks
+                if status_rank_0 != status_.Get_tag():
+                    # Log the exception with traceback
+                    interscalehub_utils.log_exception(
+                        log_message="Abnormal state : the state of Nest is "
+                                    "different between rank. Tag received: ",
+                        mpi_tag_received=status_.Get_tag())
+                    # Terminate with Error
+                    return Response.ERROR
+
+            if status_.Get_tag() == 0:
+                # wait until ready to receive new data (i.e. the sender has cleared the buffer)
+                
+                # TODO: use MPI, remove the sleep
+                # # while self.__databuffer[-1] != 1:
+                while self._data_buffer_manager.get_at(index=-1) != DATA_BUFFER_STATES.READY:
+                    time.sleep(0.001)
+                    pass
+
+                for source in range(self._num_sending):
+                    # send 'ready' to the nest rank
+                    # self._logger.info("send ready")
+                    self._comm_receiver.Send([np.array(True,dtype='b'),MPI.BOOL],dest=source,tag=0)
+                    # receive package size info
+                    self._comm_receiver.Recv([shape, 1, MPI.INT], source=source, tag=0, status=status_)
+                    # self._comm_receiver.Recv([shape, 1, MPI.INT], source=MPI.ANY_SOURCE, tag=MPI.ANY_TAG, status=status_)
+                    # NEW: receive directly into the buffer
+                    # self._comm_receiver.Recv([self.__databuffer[head_:], MPI.DOUBLE], source=source, tag=0, status=status_)
+                    data_buffer = self._data_buffer_manager.get_from(
+                                    starting_index=running_head)
+                            
+                    self._comm_receiver.Recv([data_buffer, MPI.DOUBLE],
+                                              source=source,
+                                              tag=0,
+                                              status=status_)
+                    
+                    ########################################################
+                    # TODO add call to LFPy kernel here
+                    # NOTE will be changed later to handle by rank =< 1
+                    ########################################################
+                    self._logger.debug(f"Transforming data received")
+                    times, data = self._mediator.spikes_to_rate(count,size_at_index=-2)
+                    self._logger.debug(f"data after transformation: times: {times}, data: {data}")
+                    ########################################################
+                    
+                    running_head += shape[0]  # move running head
+                # Mark as 'ready to do analysis'
+                # self.__databuffer[-1] = 0
+                self._data_buffer_manager.set_header_at(index=-1)
+
+                # important: head_ is first buffer index WITHOUT data.
+                # self.__databuffer[-2] = head_
+                self._data_buffer_manager.set_custom_value_at(
+                                                        index=-2,
+                                                        value=running_head)
+                # continue receiving the data
+                continue
+            elif status_.Get_tag() == 1:
+                # increment the count and continue receiving the data
+                count += 1
+                continue
+            elif status_.Get_tag() == 2:
+                # NOTE: simulation ended
+                # everything goes fine, terminate the loop and respond with OK
+                return Response.OK
+            else:
+                # A 'bad' MPI tag is received,
+                # log the exception with traceback
+                interscalehub_utils.log_exception(
+                    log_message="bad mpi tag :",
+                    mpi_tag_received=status_.Get_tag())
+                # terminate with Error
+                return Response.ERROR
+    
+    def _transform(self):
+        '''
+        Transforms data to LFPy signals (multiple MPI ranks possible).
+        '''
+        count = 0  # simulation/iteration step
+        # status_ = MPI.Status()
+        # self._logger.info("NESTtoTVB -- producer/sender -- Rank:"+str(self._comm_sender.Get_rank()))
+        while True:
+            # TODO: this communication has the 'rank 0' problem described in the beginning
+            accept = False
+            # while self._data_buffer_manager.get_at(index=-1) != DATA_BUFFER_STATES.HEADER:
+            #     time.sleep(0.001)
+            #     pass
+            # NOTE: calling the mediator which calls the corresponding transformer functions
+            # times,data = mediator.spike_to_rate(self.__databuffer, count)
+            # TODO: change to inject the buffer in the wrapper method of mediator
+            # times, data = spikerate.spike_to_rate(count, self.__databuffer[-2], self.__databuffer)
+            self.__logger.info("__DEBUG__ Transforming data")
+            times, data = self._mediator.spikes_to_rate(count,size_at_index=-2)
+            # TODO add call to LFPy kernel here
+            self.__logger.info(f"__DEBUG__ setting buffer ready")
+            self._data_buffer_manager.set_ready_at(index=-1)
+            self.__logger.info(f"__DEBUG__ setting buffer ready")
+            # size = np.array(int(data.shape[0]),dtype='i')
+            count += 1
+            continue
---
+++ Interscale_hub/manager_nest_to_lfpy.py
@@ -0,0 +1,136 @@
+# ------------------------------------------------------------------------------
+#  Copyright 2020 Forschungszentrum Jülich GmbH
+# "Licensed to the Apache Software Foundation (ASF) under one or more contributor
+#  license agreements; and to You under the Apache License, Version 2.0. "
+#
+# Forschungszentrum Jülich
+#  Institute: Institute for Advanced Simulation (IAS)
+#    Section: Jülich Supercomputing Centre (JSC)
+#   Division: High Performance Computing in Neuroscience
+# Laboratory: Simulation Laboratory Neuroscience
+#       Team: Multi-scale Simulation and Design
+# ------------------------------------------------------------------------------
+import numpy as np
+import os
+import time
+
+from EBRAINS_InterscaleHUB.Interscale_hub.communicator_nest_to_lfpy import CommunicatorNestLFPY
+from EBRAINS_InterscaleHUB.Interscale_hub.manager_base import InterscaleHubBaseManager
+from EBRAINS_InterscaleHUB.Interscale_hub.interscalehub_enums import DATA_EXCHANGE_DIRECTION
+from EBRAINS_RichEndpoint.application_companion.common_enums import INTERCOMM_TYPE, Response
+
+from EBRAINS_ConfigManager.global_configurations_manager.xml_parsers.default_directories_enum import DefaultDirectories
+
+
+class NestToLFPyManager(InterscaleHubBaseManager):
+    """
+        From NEST to TVB workflow manager
+    """
+    # NOTE two different sources of parameters
+    # TODO Refactoring
+    def __init__(self, parameters, configurations_manager, log_settings,
+                 sci_params_xml_path_filename=''):
+        """
+        Implements the InterscaleHubBaseManager to
+        1) Interact with InterscaleHub Facade to steer the execution
+        2) Manage the InterscaleHub functionality.
+        """
+        self.__log_settings = log_settings
+        self.__configurations_manager = configurations_manager
+        self.__logger = self.__configurations_manager.load_log_configurations(
+            name="InterscaleHub -- NEST_TO_TVB Manager",
+            log_configurations=self.__log_settings,
+            target_directory=DefaultDirectories.SIMULATION_RESULTS)
+
+        self.__logger.debug(f"host_name:{os.uname()}")
+        # 1) param stuff, create IntercommManager
+        self.__logger.debug("Init Params...")
+        super().__init__(parameters,
+                         DATA_EXCHANGE_DIRECTION.NEST_TO_TVB,
+                         self.__configurations_manager,
+                         self.__log_settings,
+                         sci_params_xml_path_filename=sci_params_xml_path_filename)
+
+        # done: TODO: set via XML settings? POD
+        # to be removed: self.__buffersize = self._max_events * 3  # 3 doubles per event
+        self.__buffersize = self._sci_params.max_events * self._sci_params.nest_buffer_size_factor
+        
+        # 2) create buffer in self.__databuffer
+        self.__logger.debug("Creating MPI shared memory Buffer...")
+        self.__databuffer = self._get_mpi_shared_memory_buffer(self.__buffersize)
+        self.__logger.info("Buffer created.")
+
+        # 3) Data channel setup
+        self.__logger.info("setting up data channels...")
+        self.__data_channel_setup()
+        self.__logger.info("data channels open and ready.")
+
+    def __data_channel_setup(self):
+        """
+        Open ports and register connection details.
+        Accept connection on ports and create INTER communicators.
+
+        MVP: register = write port details to file.
+        MVP: Two connections
+            - input = incoming simulation data
+            - output = outgoing simulation data
+        """
+        # NOTE: create port files and make connection
+        # In Demo example: producer/Consumer are inherited from mpi_io_extern,
+        # and then they are started as threads which then call mpi_io_extern run() method
+        # which then calls make_connection() method
+
+        if self._intra_comm.Get_rank() == 0:
+            self.__input_comm, self.__input_port = self._set_up_connection(
+                direction=DATA_EXCHANGE_DIRECTION.NEST_TO_LFPY.name,
+                intercomm_type=INTERCOMM_TYPE.RECEIVER.name)
+            self.__output_comm = None
+
+        else:
+            self.__output_comm, self.__output_port = self._set_up_connection(
+                direction=DATA_EXCHANGE_DIRECTION.NEST_TO_LFPY.name,
+                intercomm_type=INTERCOMM_TYPE.SENDER.name)
+            self.__input_comm = None
+
+    def start(self):
+        """
+       implementation of abstract method to start transformation and
+        exchanging the data with TVB and NEST.
+        """
+        self.__logger.info("Start data transfer and usecase science...")
+        # initialize Communicator
+        #
+        # TO BE FIXED: Instance attribute __nest_lfpy_communicator defined outside __init__
+        #
+        self.__nest_lfpy_communicator = CommunicatorNestLFPY(
+            self.__configurations_manager,
+            self.__log_settings,
+            self._interscalehub_buffer_manager,
+            self._mediator)
+
+        # start exchanging the data
+        if self.__nest_lfpy_communicator.start(self._intra_comm,
+                                              self.__input_comm,
+                                              self.__output_comm) == Response.ERROR:
+            # Case a: something went wrong during the data exchange
+            # NOTE the details are already been logged at the origin of the error
+            # now terminate with error
+            self.__logger.critical('Got error while exchanging the data.')
+            return Response.ERROR
+        else:
+            # Case b: everything went well
+            return Response.OK
+
+    def stop(self):
+        """
+        implementation of the abstract method to conclude the pivot operations
+        and stop exchanging the data.
+
+        TODO: add error handling and fail checks
+        """
+        self.__logger.info("Stop InterscaleHub and disconnect...")
+        self.__nest_lfpy_communicator.stop()
+        if self._intra_comm.Get_rank() == 0:
+            self._intercomm_manager.close_and_finalize(self.__input_comm, self.__input_port)
+        else:
+            self._intercomm_manager.close_and_finalize(self.__output_comm, self.__output_port)
